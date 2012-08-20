2128cf511a92804df349bdcc24679008479a8d7d
Cruz Monrreal II <Cruz.Monrreal@gmail.com>
2012-08-20 01:24:45 -0500

    Used Serial1 over SoftwareSerial. SimpleSerialResponse and AsyncResponse working, rudimentary implementation.
    Signed-off-by: Cruz Monrreal II <Cruz.Monrreal@gmail.com>

--- Sphero.cpp
+++ Sphero.cpp
@@ -10,45 +10,62 @@
 #include <Arduino.h>
 #include "Sphero.h"
 
-Sphero::Sphero(SoftwareSerial &serial){
-    bluetooth = &serial;
+Sphero::Sphero(){
+    //bluetooth = &serial;
+    Serial1.begin(9600);
+    mrsp = 0x00;
 }
 
 Sphero::~Sphero(){}
 
 //uint8_t Sphero::boost(uint16_t heading, uint8_t duration);
-void Sphero::roll(short heading, char speed){
-    sendSynchronousPacket(0x02, 0x30, 0x02, 0x05, speed, heading >> 8 , heading, 0xFF);
+char Sphero::roll(short heading, char speed){
+    return sendSynchronousPacket(0x02, 0x30, 0x02, 0x05, speed, heading >> 8 , heading, 0xFF);
 }
 //void Sphero::stop();
-void Sphero::setRGBColor(char red, char green, char blue){
-    sendSynchronousPacket(0x02, 0x20, 0x01, 0x05, red, green, blue, 0xFF);
+char Sphero::setRGBColor(char red, char green, char blue){
+    return sendSynchronousPacket(0x02, 0x20, 0x01, 0x05, red, green, blue, 0xFF);
 }
-void Sphero::getRGBColor(){
-    sendSynchronousPacket(0x02, 0x22, 0x06, 0x01);
+char Sphero::getRGBColor(){
+    return sendSynchronousPacket(0x02, 0x22, 0x06, 0x01);
 }
-void Sphero::setBackLED(char intensity){
-    sendSynchronousPacket(0x02, 0x21, 0x04, 0x02, intensity);
+char Sphero::setBackLED(char intensity){
+    return sendSynchronousPacket(0x02, 0x21, 0x04, 0x02, intensity);
 }
 //void Sphero::setRotationRate(uint8_t rate);
-void Sphero::rotateHeadingBy(short heading){
-    sendSynchronousPacket(0x02, 0x01, 0x03, 0x03, heading >> 8 , heading);
+char Sphero::rotateHeadingBy(short heading){
+    return sendSynchronousPacket(0x02, 0x01, 0x03, 0x03, heading >> 8 , heading);
 }
 //void Sphero::setMotionTimeout();
 //void Sphero::setRawMotorValues(uint8_t l_mode, uint8_t r_mode, uint8_t l_pwr, uint8_t r_pwr);
 
-void Sphero::sendSynchronousPacket(char DID, char CID, char SEQ, char DLEN, ...){
+char Sphero::setStabilization(char enable){
+    return sendSynchronousPacket(0x02, 0x02, 0x09, 0x02, enable==1);
+}
+    
+char Sphero::setStreamingData(short sample_freq_divisor, short frames_per_packet, long int mask){
+    return sendSynchronousPacket(0x02, 0x11, 0x07, 0x0A, (400/sample_freq_divisor) >> 8, (400/sample_freq_divisor), frames_per_packet >> 8, frames_per_packet, char(mask >> 24), char(mask >> 16), char(mask >> 8), char(mask), 5);
+}
+
+char Sphero::getOptionFlags(){
+    return sendSynchronousPacket(0x02, 0x36, 0x08, 0x01);
+}
+
+char Sphero::sendSynchronousPacket(char DID, char CID, char SEQ, char DLEN, ...){
     short i=0;
     char sum=0, data;
     va_list args;
     
+    // Flush pipe (just in case)
+    //Serial1.flush();
+    
     // Write data
-    bluetooth->write(char(0xFF));
-    bluetooth->write(char(0xFF));
-    bluetooth->write(char(DID));
-    bluetooth->write(char(CID));
-    bluetooth->write(char(SEQ));
-    bluetooth->write(char(DLEN));
+    Serial1.write(char(0xFF));
+    Serial1.write(char(0xFF));
+    Serial1.write(char(DID));
+    Serial1.write(char(CID));
+    Serial1.write(char(SEQ));
+    Serial1.write(char(DLEN));
     
     // Calculate checksum
     sum += DID + CID + SEQ + DLEN;
@@ -56,43 +73,109 @@ void Sphero::sendSynchronousPacket(char DID, char CID, char SEQ, char DLEN, ...)
     va_start(args, DLEN);
     for(; i<DLEN-1; i++){
         data = va_arg(args, int);
-        bluetooth->write(char(data));
+        Serial1.write(data);
+        
+        Serial.print(String(data, HEX));
+        Serial.print(" ");
         sum += data;
     }
     va_end(args);
+    Serial.println();
     
     // Write Checksum
-    bluetooth->write(char(~sum));
+    Serial1.write(char(~sum));
     
-    // Wait for response
-    readResponsePacket();
+    // Wait for Simple Response
+    return readResponsePacket();
 }
 
-void Sphero::readResponsePacket(){
-    char ctr = 0;
+char Sphero::readResponsePacket(){
+    int i, len;
     response = "";
     
-    bluetooth->flush(); // Get rid of any stale data
+    Serial1.flush(); // Get rid of any stale data
     
-    while(!bluetooth->available()){}        // Wait for response packet to begin
+    Serial.println("Waiting for Simple Response");
+    while(Serial1.available() < 5);
     
-    response += char(bluetooth->read());    // Append first character
+    Serial1.read();         // 0xFF
+    Serial1.read();         // 0xFF
+    Serial.print(" MRSP: ");
+    mrsp = Serial1.read();  // MRSP
+    Serial.print(String(mrsp, HEX));
+    Serial.println();
+    Serial.print(" SEQ: ");
+    Serial.println(Serial1.read());     // SEQ
+    Serial.print(" DLEN: ");
+    len = Serial1.read();   // DLEN
+    Serial.println(len, HEX);
     
-    while(ctr < 32  ){    // Arbritrary value
-        if (bluetooth->available()){
-            response += char(bluetooth->read());
-            ctr = 0;
-        }else{
-            ctr++;
-        }
-    }
+    Serial.println(" Data:");
+    Serial.print("  ");
+    for(i=0; i<len-1; i++){
+        char data; 
+        while(!Serial1.available());
+        
+        data = char(Serial1.read());
+        Serial.print(String(data, HEX));
+        Serial.print(" ");
+        response+=data;
+    }    
+    Serial.println();
+    
+    Serial.print(" Checksum: ");
+    while(!Serial1.available());
+    Serial.println(Serial1.read());     // CHK
+    
+    Serial1.flush();
+    
+    delay(10);
+    
+    return mrsp;
 }
 
-String Sphero::readRawResponse(){ return response; }
-
-String Sphero::readResponseData(){
-    if (response.charAt(4) > 1)
-        return response.substring(5, 5 + (response.charAt(4)-1));
-    return "";
+char Sphero::getMRSP(){
+    return mrsp;
 }
+
+String Sphero::readAsyncPacket(){
+    int i, len;
+    response = "";
+    
+    Serial.println("Waiting for Async Packet");
+    while(Serial1.available() < 5);
+    
+    Serial1.read();         // 0xFF
+    Serial1.read();         // 0xFE
+    Serial.print(" ID Code: ");
+    Serial.println(Serial1.read());         // ID Code
+    Serial.print(" DLEN: ");
+    len = Serial1.read() << 8;  // DLEN-MSB
+    len += Serial1.read();      // DLEN-LSB
+    Serial.println(len);
+    
+    Serial.println(" Data:");
+    for(i=0; i<len-1; i++){
+        char data; 
+        while(!Serial1.available());
+        
+        data = char(Serial1.read());
+        Serial.print(String(data, HEX));
+        Serial.print(" ");
+        response+=data;
+    }    
+    Serial.println();
+    
+    Serial.print(" Checksum: ");
+    while(!Serial1.available());
+    Serial.println(Serial1.read());     // CHK
+    
+    Serial1.flush();
+    
+    delay(10);
+    
+    return response;
+}
+
+String Sphero::readResponse(){ return response; }
   
--- Sphero.h
+++ Sphero.h
@@ -10,38 +10,46 @@
 #define Sphero_h
 
 #include <stdint.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
+#include <Arduino.h>
+#include <Serial.h>
 
 class Sphero{
 
 public:
   // public methods
-  Sphero( SoftwareSerial &serial );
+  Sphero(/* SoftwareSerial &serial */);
   ~Sphero();
   
   // Set
   //void boost(uint16_t heading, uint8_t duration);
-  void roll( short heading, char speed );
+  char roll( short heading, char speed );
   //void stop();
-  void setRGBColor( char red,  char green,  char blue );
-  void getRGBColor( void );
-  void setBackLED( char intensity);
+  char setRGBColor( char red,  char green,  char blue );
+  char getRGBColor( void );
+  char setBackLED( char intensity);
   //void setRotationRate(uint8_t rate);
-  void rotateHeadingBy( short heading );
+  char rotateHeadingBy( short heading );
+  char getOptionFlags( void );
   //void setMotionTimeout();
   //void setRawMotorValues(uint8_t l_mode, uint8_t r_mode, uint8_t l_pwr, uint8_t r_pwr);	// Special note. See Datasheet.
   
   // Get
   //void getHeading(uint16_t* data);
-
+  char setStreamingData( short sample_freq_divisor, short frames_per_packet, long mask );
+  char setStabilization( char enable );
   
-  String readRawResponse( void );
-  String readResponseData( void );
+  String readResponse( void );
+  String readAsyncPacket( void );
+  
+  char getMRSP( void );
 private:
-  void sendSynchronousPacket( char DID, char CID, char SEQ, char DLEN, ... );
-  void readResponsePacket( void );
-  SoftwareSerial *bluetooth;
+  char sendSynchronousPacket( char DID, char CID, char SEQ, char DLEN, ... );
+  char readResponsePacket( void );
+  
+  //Serial *bluetooth;
   String response;
+  char mrsp;
 };
 
 #endif
